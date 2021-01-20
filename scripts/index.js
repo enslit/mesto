8294ea89790aa64d4ee70efb01f95c2b67c5f72d
Aleksandr <alex@enslit.ru>
2021-01-20 16:59:12 +0300

    feature(cards): Реализовано добавление новой карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,25 @@
 import {initialCards} from './cards.js'
 
+const popupTemplate = document.querySelector('#popupTemplate').content
+const cardTemplate = document.querySelector('#cardTemplate').content
 const profile = document.querySelector('.profile')
 const name = profile.querySelector('.profile__name')
 const about = profile.querySelector('.profile__about')
-// const popup = document.querySelector('.popup')
-// const form = popup.querySelector('.form')
-// const nameInput = form.querySelector('.form__input_type_name')
-// const aboutInput = form.querySelector('.form__input_type_about')
 const btnEditProfile = profile.querySelector('.btn_type_edit-profile')
-// const btnClosePopup = popup.querySelector('.btn_type_close')
-
-// Cards
+const btnAddCard = profile.querySelector('.btn_type_add-card')
 const cardsList = document.querySelector('.cards__list')
-const cardTemplate = document.querySelector('#cardTemplate').content
 
-const closePopup = () => popup.classList.remove('popup_opened')
-
-const openPopup = () => {
-	nameInput.value = name.textContent
-	aboutInput.value = about.textContent
-	popup.classList.add('popup_opened')
+const init = () => {
+	initialCards.forEach(addCard)
 }
 
-const handleFormSubmit = event => {
-	event.preventDefault()
+const closePopup = (event) => {
+	const popup = event.target.closest('.popup')
+	const close = popup.querySelector('.btn_type_close')
 
-	name.textContent = nameInput.value
-	about.textContent = aboutInput.value
-
-	closePopup()
+	close.removeEventListener('click', closePopup)
+	popup.classList.remove('popup_opened')
+	popup.remove()
 }
 
 const addCard = (card) => {
@@ -44,12 +35,118 @@ const addCard = (card) => {
 	cardsList.prepend(cardElement)
 }
 
-const init = () => {
-	initialCards.forEach(addCard)
+const createInput = (attrs) => {
+	const {
+		type = 'text',
+		name = '',
+		value = '',
+		placeholder = '',
+		className = 'form__input',
+		modsClassName = []
+	} = attrs
+
+	const classes = modsClassName
+		? className + ' ' + modsClassName.join(' ')
+		: className
+
+	return `<input type="${type}" name="${name}" value="${value}" class="${classes}" placeholder="${placeholder}">`
 }
 
-// form.addEventListener('submit', handleFormSubmit)
-// btnEditProfile.addEventListener('click', openPopup)
-// btnClosePopup.addEventListener('click', closePopup)
+const openEditProfilePopup = () => {
+	const profilePopupElement = popupTemplate.cloneNode(true)
+	const popup = profilePopupElement.querySelector('.popup')
+	const form = profilePopupElement.querySelector('.form')
+	const title = profilePopupElement.querySelector('.popup__title')
+	const btnClosePopup = profilePopupElement.querySelector('.btn_type_close')
+
+	form.name = 'edit-profile'
+	title.textContent = 'Редактирование профиля'
+
+	const nameInput = createInput({
+		name: 'name',
+		value: name.textContent,
+		modsClassName: ['form__input_type_name']
+	})
+
+	const aboutInput = createInput({
+		name: 'about',
+		value: about.textContent,
+		modsClassName: ['form__input_type_about']
+	})
+
+	form.insertAdjacentHTML('afterbegin', nameInput + aboutInput)
+
+	popup.classList.add('popup_opened')
+
+	btnClosePopup.addEventListener('click', closePopup)
+	form.addEventListener('submit', handleProfileFormSubmit)
+
+	document.body.append(profilePopupElement)
+}
+
+const openAddCardPopup = () => {
+	const CardPopupElement = popupTemplate.cloneNode(true)
+	const popup = CardPopupElement.querySelector('.popup')
+	const title = CardPopupElement.querySelector('.popup__title')
+	const form = CardPopupElement.querySelector('.form')
+	const btnClosePopup = CardPopupElement.querySelector('.btn_type_close')
+
+	form.name = 'add-card'
+	title.textContent = 'Новое место'
+
+	const nameInput = createInput({
+		name: 'name',
+		placeholder: 'Название',
+		modsClassName: ['form__input_type_name']
+	})
+
+	const linkInput = createInput({
+		name: 'link',
+		placeholder: 'Ссылка на картинку',
+		modsClassName: ['form__input_type_link']
+	})
+
+	form.insertAdjacentHTML('afterbegin', nameInput + linkInput)
+
+	popup.classList.add('popup_opened')
+
+	btnClosePopup.addEventListener('click', closePopup)
+	form.addEventListener('submit', handleAddCardFormSubmit)
+
+	document.body.append(CardPopupElement)
+}
+
+const handleProfileFormSubmit = event => {
+	event.preventDefault()
+
+	const form = event.target
+	const nameInput = form.querySelector('.form__input_type_name')
+	const aboutInput = form.querySelector('.form__input_type_about')
+
+	name.textContent = nameInput.value
+	about.textContent = aboutInput.value
+
+	form.removeEventListener('submit', handleProfileFormSubmit)
+	closePopup(event)
+}
+
+const handleAddCardFormSubmit = event => {
+	event.preventDefault()
+
+	const form = event.target
+	const nameInput = form.querySelector('.form__input_type_name')
+	const linkInput = form.querySelector('.form__input_type_link')
+
+	addCard({
+		name: nameInput.value,
+		link: linkInput.value,
+	})
+
+	form.removeEventListener('submit', handleAddCardFormSubmit)
+	closePopup(event)
+}
+
+btnEditProfile.addEventListener('click', openEditProfilePopup)
+btnAddCard.addEventListener('click', openAddCardPopup)
 
 init()
