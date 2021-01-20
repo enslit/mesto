2982704839499c597458181657675d3f8d48ee6b
Aleksandr <alex@enslit.ru>
2021-01-20 15:16:36 +0300

    feature: Инициализация карточек из скрипта

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,18 @@
+import {initialCards} from './cards.js'
+
 const profile = document.querySelector('.profile')
 const name = profile.querySelector('.profile__name')
 const about = profile.querySelector('.profile__about')
-const popup = document.querySelector('.popup')
-const form = popup.querySelector('.form')
-const nameInput = form.querySelector('.form__input_type_name')
-const aboutInput = form.querySelector('.form__input_type_about')
+// const popup = document.querySelector('.popup')
+// const form = popup.querySelector('.form')
+// const nameInput = form.querySelector('.form__input_type_name')
+// const aboutInput = form.querySelector('.form__input_type_about')
 const btnEditProfile = profile.querySelector('.btn_type_edit-profile')
-const btnClosePopup = popup.querySelector('.btn_type_close')
+// const btnClosePopup = popup.querySelector('.btn_type_close')
+
+// Cards
+const cardsList = document.querySelector('.cards__list')
+const cardTemplate = document.querySelector('#cardTemplate').content
 
 const closePopup = () => popup.classList.remove('popup_opened')
 
@@ -25,6 +31,25 @@ const handleFormSubmit = event => {
 	closePopup()
 }
 
-form.addEventListener('submit', handleFormSubmit)
-btnEditProfile.addEventListener('click', openPopup)
-btnClosePopup.addEventListener('click', closePopup)
+const addCard = (card) => {
+	const cardElement = cardTemplate.cloneNode(true)
+
+	const cardImg = cardElement.querySelector('.card__image')
+	const cardTitle = cardElement.querySelector('.card__title')
+
+	cardImg.src = card.link
+	cardImg.alt = card.name
+	cardTitle.textContent = card.name
+
+	cardsList.prepend(cardElement)
+}
+
+const init = () => {
+	initialCards.forEach(addCard)
+}
+
+// form.addEventListener('submit', handleFormSubmit)
+// btnEditProfile.addEventListener('click', openPopup)
+// btnClosePopup.addEventListener('click', closePopup)
+
+init()
