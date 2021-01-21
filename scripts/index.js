3efd3f4f7052b91136a89609f5a76e5fd3ec57f6
Aleksandr <alex@enslit.ru>
2021-01-21 15:52:27 +0300

    refactor(javascript): Разбивка на меньшие функции

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,45 @@
 import {initialCards} from './cards.js'
 
-const popupTemplate = document.querySelector('#popupTemplate').content
 const cardTemplate = document.querySelector('#cardTemplate').content
-const cardImageTemplate = document.querySelector('#popupImageTemplate').content
 const profile = document.querySelector('.profile')
+const cardsList = document.querySelector('.cards__list')
 const name = profile.querySelector('.profile__name')
 const about = profile.querySelector('.profile__about')
-const btnEditProfile = profile.querySelector('.btn_type_edit-profile')
-const btnAddCard = profile.querySelector('.btn_type_add-card')
-const cardsList = document.querySelector('.cards__list')
+
+const timeout = 500
 
 const init = () => {
 	initialCards.forEach(addCard)
+}
+
+const initPopup = (content, title = '', type = '') => {
+	const popup = createPopup(content, title, type)
+	document.body.append(popup)
+	openPopup()
+}
+
+const getTemplateElement = (selector) => {
+	const template = document.querySelector(selector).content
+	return template.cloneNode(true)
+}
+
+const getInputValue = (form, field) => form.querySelector(field).value
+
+const openPopup = () => {
+	const popup = document.querySelector('.popup')
+	popup.addEventListener('click', closePopup)
+	popup.classList.add('popup_opened')
+}
+
+const closePopup = ({type, target}) => {
+	if (type === 'submit' || target.classList.contains('btn_type_close') || target.classList.contains('popup')) {
+		const popup = target.closest('.popup')
+		popup.classList.remove('popup_opened')
+
+		setTimeout(() => {
+			popup.remove()
+		}, timeout)
+	}
 }
 
 const createCard = (link, name) => {
@@ -51,131 +79,116 @@ const createInput = (attrs) => {
 	return input
 }
 
-const createForm = (name, submitHandler, fields = []) => {
+const createForm = (name, submitHandler) => {
 	const formTemplate = document.querySelector('#form').content
 	const formElement = formTemplate.cloneNode(true)
 	const form = formElement.querySelector('.form')
 
-	formElement.name = name
-
-	fields.reverse().forEach(field => {
-		const input = createInput(field)
-		form.prepend(input)
-	})
-
+	form.name = name
 	form.addEventListener('submit', submitHandler)
 
 	return formElement
 }
 
-const createPopup = (title, withForm = false, formOptions = {}) => {
-	const popupElement = popupTemplate.cloneNode(true)
-	const $popup = popupElement.querySelector('.popup')
-	const $popupContainer = popupElement.querySelector('.popup__container')
-	const $title = popupElement.querySelector('.popup__title')
-	const $btnClosePopup = popupElement.querySelector('.btn_type_close')
+const createEditProfileForm = () => {
+	const form = createForm('edit-profile', onSubmitEditProfile)
 
-	$title.textContent = 'Редактирование профиля'
+	const inputName = createInput({
+		name: 'name',
+		value: name.textContent,
+		modsClassName: ['form__input_type_name']
+	})
+	const inputAbout = createInput({
+		name: 'about',
+		value: about.textContent,
+		modsClassName: ['form__input_type_about']
+	})
 
-	if (withForm) {
-		const {
-			name = 'form',
-			submitHandler = (e) => console.log(`Default handler for ${e.target}`),
-			fields = []
-		} = formOptions
-		const form = createForm(name, submitHandler, fields)
-		$popupContainer.append(form)
+	putInputToForm(form, inputName)
+	putInputToForm(form, inputAbout)
+
+	return form
+}
+
+const createAddCardForm = () => {
+	const form = createForm('add-card', onSubmitAddCard)
+
+	const inputName = createInput({
+		name: 'name',
+		placeholder: 'Название',
+		modsClassName: ['form__input_type_name']
+	})
+	const inputLink = createInput({
+		name: 'link',
+		placeholder: 'Ссылка на картинку',
+		modsClassName: ['form__input_type_link']
+	})
+
+	putInputToForm(form, inputName)
+	putInputToForm(form, inputLink)
+
+	return form
+}
+
+const createImage = (src, alt) => {
+	const img = document.createElement('img')
+	img.src = src
+	img.alt = alt
+	img.classList.add('popup__image')
+	return img
+}
+
+const createSignIn = (text) => {
+	const el = document.createElement('h2')
+	el.classList.add('popup__image-sign')
+	el.textContent = text
+	return el
+}
+
+const createPopup = (content, title, type) => {
+	const popupElement = getTemplateElement('#popupTemplate')
+	const popup = popupElement.querySelector('.popup')
+	const container = popupElement.querySelector('.popup__container')
+
+	switch (type) {
+		case 'img':
+			popupElement.querySelector('.popup__title').remove()
+			container.classList.add('popup__container_image')
+			break
+		default:
+			const title = popupElement.querySelector('.popup__title')
+			title.textContent = title
 	}
 
-	$popup.classList.add('popup_opened')
+	putContentToPopup(content, container)
 
-	$btnClosePopup.addEventListener('click', closePopup)
+	popup.addEventListener('click', closePopup)
 
 	return popupElement
 }
 
+const putInputToForm = (form, input) => form.querySelector('.form__inputs').append(input)
+
+const putContentToPopup = (content, container) => {
+	if (Array.isArray(content)) {
+		content.forEach(el => {
+			container.append(el)
+		})
+	} else {
+		container.append(content)
+	}
+}
+
 const addCard = ({link, name}) => {
-	const card = createCard(link, name)
-
-	const cardImg = card.querySelector('.card__image')
-	const likeButton = card.querySelector('.btn_type_like')
-	const deleteButton = card.querySelector('.card__delete')
-
-	cardImg.addEventListener('click', onClickCard)
-	likeButton.addEventListener('click', onClickLike)
-	deleteButton.addEventListener('click', onClickDelete)
-
-	cardsList.prepend(card)
-}
-
-const closePopup = (event) => {
-	const popup = event.target.closest('.popup')
-	const close = popup.querySelector('.btn_type_close')
-
-	close.removeEventListener('click', closePopup)
-	popup.classList.remove('popup_opened')
-	popup.remove()
-}
-
-const openEditProfilePopup = () => {
-	const title = 'Редактирование профиля'
-
-	const nameField = {
-		name: 'name',
-		value: name.textContent,
-		modsClassName: ['form__input_type_name']
-	}
-	const aboutField = {
-		name: 'about',
-		value: about.textContent,
-		modsClassName: ['form__input_type_about']
-	}
-	const fields = [nameField, aboutField]
-
-	const formOptions = {
-		name: 'edit-profile',
-		submitHandler: onSubmitEditProfile,
-		fields
-	}
-
-	const popup = createPopup(title, true, formOptions)
-	document.body.append(popup)
-}
-
-const openAddCardPopup = () => {
-	const title = 'Новое место'
-
-	const nameField = {
-		name: 'name',
-		placeholder: 'Название',
-		modsClassName: ['form__input_type_name']
-	}
-	const linkField = {
-		name: 'link',
-		placeholder: 'Ссылка на картинку',
-		modsClassName: ['form__input_type_link']
-	}
-	const fields = [nameField, linkField]
-
-	const formOptions = {
-		name: 'add-card',
-		submitHandler: onSubmitAddCard,
-		fields
-	}
-
-	const popup = createPopup(title, true, formOptions)
-	document.body.append(popup)
+	cardsList.prepend(createCard(link, name))
 }
 
 const onSubmitEditProfile = (event) => {
 	event.preventDefault()
 
 	const form = event.target
-	const nameInput = form.querySelector('.form__input_type_name')
-	const aboutInput = form.querySelector('.form__input_type_about')
-
-	name.textContent = nameInput.value
-	about.textContent = aboutInput.value
+	name.textContent = getInputValue(form, '.form__input_type_name')
+	about.textContent = getInputValue(form, '.form__input_type_about')
 
 	form.removeEventListener('submit', onSubmitEditProfile)
 	closePopup(event)
@@ -185,45 +198,45 @@ const onSubmitAddCard = (event) => {
 	event.preventDefault()
 
 	const form = event.target
-	const nameInput = form.querySelector('.form__input_type_name')
-	const linkInput = form.querySelector('.form__input_type_link')
+	const name = getInputValue(form, '.form__input_type_name')
+	const link = getInputValue(form, '.form__input_type_link')
 
-	addCard({
-		name: nameInput.value,
-		link: linkInput.value,
-	})
+	addCard({name, link})
 
 	form.removeEventListener('submit', onSubmitAddCard)
 	closePopup(event)
 }
 
-const onClickLike = (event) => {
-	event.target.classList.toggle('btn_type_like-active')
+const onClickProfile = ({target}) => {
+	if (target.classList.contains('btn_type_edit-profile')) {
+		initPopup(createEditProfileForm(), 'Редактирование профиля')
+	}
+
+	if (target.classList.contains('btn_type_add-card')) {
+		initPopup(createAddCardForm(),'Новое место')
+	}
 }
 
-const onClickDelete = (event) => {
-	event.target.closest('.card').parentElement.remove()
+const onClickCardList = ({target}) => {
+	if (target.classList.contains('btn_type_like')) {
+		target.classList.toggle('btn_type_like-active')
+	}
+
+	if (target.classList.contains('card__delete')) {
+		target.closest('.card').parentElement.remove()
+	}
+
+	if (target.classList.contains('card__image')) {
+		const title = target.closest('.card').querySelector('.card__title').textContent
+		const content = [
+			createImage(target.src, title),
+			createSignIn(title)
+		]
+		initPopup(content, '', 'img')
+	}
 }
 
-const onClickCard = (event) => {
-	const ImagePopupElement = cardImageTemplate.cloneNode(true)
-	const popup = ImagePopupElement.querySelector('.popup')
-	const img = ImagePopupElement.querySelector('.popup__image')
-	const sign = ImagePopupElement.querySelector('.popup__image-sign')
-	const btnClosePopup = ImagePopupElement.querySelector('.btn_type_close')
-
-	const link = event.target.src
-	const title = event.target.closest('.card').querySelector('.card__title').textContent
-	img.src = link
-	sign.textContent = title
-
-	popup.classList.add('popup_opened')
-
-	btnClosePopup.addEventListener('click', closePopup)
-	document.body.append(ImagePopupElement)
-}
-
-btnEditProfile.addEventListener('click', openEditProfilePopup)
-btnAddCard.addEventListener('click', openAddCardPopup)
+profile.addEventListener('click', onClickProfile, true)
+cardsList.addEventListener('click', onClickCardList, true)
 
 init()
