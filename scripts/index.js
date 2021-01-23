c83d3c5f1117c5739efefebf79cba13c4bf2393a
Aleksandr <alex@enslit.ru>
2021-01-23 15:22:42 +0300

    refactor: Данные карточек вынесены в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -16,33 +16,6 @@ const addCardForm = addCardPopup.querySelector('.form')
 const cardTemplate = document.querySelector('#cardTemplate').content
 const cardList = document.querySelector('.cards__list')
 
-const initialCards = [
-	{
-		name: 'Архыз',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-	},
-	{
-		name: 'Челябинская область',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-	},
-	{
-		name: 'Иваново',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-	},
-	{
-		name: 'Камчатка',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-	},
-	{
-		name: 'Холмогорский район',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-	},
-	{
-		name: 'Байкал',
-		link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-	}
-]
-
 const closePopup = (event) => {
 	const popup = event.target.closest('.popup')
 	popup.classList.remove('popup_opened')
