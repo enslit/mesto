21a3e245dc6b12aa06fd89b9ae477c55b43a531e
Aleksandr <alex@enslit.ru>
2021-02-02 14:43:11 +0300

    refactor: Константы вынесены в отдельный файл

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,3 @@
-const cardTemplate = document.querySelector('#cardTemplate').content
-const profile = document.querySelector('.profile')
-const name = profile.querySelector('.profile__name')
-const about = profile.querySelector('.profile__about')
-const cardsList = document.querySelector('.cards__list')
-// Всплывающие окна
-const popupEditProfile = document.querySelector('.popup_type_edit-profile')
-const popupAddCard = document.querySelector('.popup_type_add-card')
-const popupPreviewImage = document.querySelector('.popup_type_image')
-const imgPreview = popupPreviewImage.querySelector('.popup__image')
-const signPreview = popupPreviewImage.querySelector('.popup__sign')
-// Кнопки
-const btnEditProfile = profile.querySelector('.btn_type_edit-profile')
-const btnAddCard = profile.querySelector('.btn_type_add-card')
-// Формы и их элементы
-const formAddCard = popupAddCard.querySelector('.form')
-const formEditProfile = popupEditProfile.querySelector('.form')
-const inputName = formEditProfile.querySelector('.form__input_type_name')
-const inputAbout = formEditProfile.querySelector('.form__input_type_about')
-inputName.value = name.textContent
-inputAbout.value = about.textContent
-const inputNameCard = formAddCard.querySelector('.form__input_type_card-name')
-const inputLink = formAddCard.querySelector('.form__input_type_link')
-
 // Скрытие переданного всплывающего окна
 const closePopup = (popup) => {
 	removeListenersPopup(popup)
