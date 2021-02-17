640925f19d94c1b51b1fe6df0d50ab58beae3a5d
Aleksandr <alex@enslit.ru>
2021-02-17 17:54:58 +0300

    feature(validator-class): Скелет класса Validator

---
+++ scripts/classes/Validator.js
@@ -0,0 +1,39 @@
+class Validator {
+  constructor(selectors, formElement) {
+    this._formElement = formElement
+    this._formSelector = selectors.formSelector
+    this._inputSelector = selectors.inputSelector
+    this._submitButtonSelector = selectors.submitButtonSelector
+    this._inactiveButtonClass = selectors.inactiveButtonClass
+    this._inputErrorClass = selectors.inputErrorClass
+    this._errorClass = selectors.errorClass
+  }
+
+  _showInputError() {
+
+  }
+
+  _hideInputError() {
+
+  }
+
+  _getElementError() {
+
+  }
+
+  _checkValidity() {
+
+  }
+
+  _hasInvalidInput() {
+
+  }
+
+  _toggleButtonState() {
+
+  }
+
+  enableValidation() {
+
+  }
+}
