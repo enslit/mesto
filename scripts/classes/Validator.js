116102dd8aeb3d90a6f69e3b4825afcd4ee91434
Aleksandr <alex@enslit.ru>
2021-02-17 18:01:05 +0300

    feature(validator-class): Метод переключения состояния кнопки сабмита

--- scripts/classes/Validator.js
+++ scripts/classes/Validator.js
@@ -1,12 +1,11 @@
 class Validator {
   constructor(selectors, formElement) {
     this._formElement = formElement
-    this._formSelector = selectors.formSelector
-    this._inputSelector = selectors.inputSelector
-    this._submitButtonSelector = selectors.submitButtonSelector
     this._inactiveButtonClass = selectors.inactiveButtonClass
     this._inputErrorClass = selectors.inputErrorClass
     this._errorClass = selectors.errorClass
+    this._submitButton = formElement.querySelector(selectors.submitButtonSelector)
+    this._inputs = Array.from(formElement.querySelectorAll(selectors.inputSelector))
   }
 
   _showInputError() {
@@ -30,10 +29,16 @@ class Validator {
   }
 
   _toggleButtonState() {
-
+    if (this._hasInvalidInput(this._inputs)) {
+      this._submitButton.classList.add(this._inactiveButtonClass)
+      this._submitButton.disabled = true
+    } else {
+      this._submitButton.classList.remove(this._inactiveButtonClass)
+      this._submitButton.disabled = false
+    }
   }
 
   enableValidation() {
-
+    this._toggleButtonState()
   }
 }
