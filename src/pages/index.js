105952fd55143f17a6ebb1f63ec169689afd160c
Aleksandr <alex@enslit.ru>
2021-03-19 16:46:38 +0300

    feature(popupClass): Реализация попапа с формой редактирования профиля через класс

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import {initialCards} from '../scripts/cards.js'
 import {
+	popupPreviewPicture,
 	cardTemplateSelector,
-	popupElements,
 	cardsListSelector,
-	buttonElements,
-	formEditProfileElements,
-	profileElements,
-	validateOptions,
 	popupAddCardSelector,
+	popupEditProfileSelector,
+	buttonEditProfile,
+	buttonAddCard,
+	validateOptions,
 } from '../scripts/constants.js'
 import Card from '../components/Card.js'
 import FormValidator from '../components/FormValidator.js'
@@ -15,25 +15,14 @@ import Section from '../components/Section.js'
 import './index.css'
 import PopupWithForm from '../components/PopupWithForm'
 
-// Обработчик клика кнопки открытия всплывающего окна с форой редактирования профиля
-const openEditProfilePopup = () => {
-	popupElements.editProfile.open()
-}
-
 // Callback обработчика клика на изображение
 export const openPreviewPicture = (data) => {
-	popupElements.previewPicture.open(data)
+	popupPreviewPicture.open(data)
 }
 
 // Обработчик события отправки формы редактирования профиля
-const handleProfileFormSubmit = event => {
-	event.preventDefault()
-
-	// Перезапишем текст в элементе name и about на странице
-	profileElements.name.textContent = formEditProfileElements.name.value
-	profileElements.about.textContent = formEditProfileElements.about.value
-
-	popupElements.editProfile.close()
+const handleProfileFormSubmit = (values) => {
+	// TODO SetUserInfo
 }
 
 // Обработчик события отправки формы добавления новой карточки
@@ -49,6 +38,14 @@ const createCard = (cardData) => {
 	return card.getCard()
 }
 
+// Включает валидацию формы в всплывающем окне
+const enableValidation = (popup, options) => {
+	const form = popup.getElement().querySelector('.form')
+	const validator = new FormValidator(options, form)
+	validator.enableValidation()
+	return validator
+}
+
 // Создаем объект контейнера стартовых карточек
 const cardList = new Section({
 	items: initialCards,
@@ -62,17 +59,18 @@ cardList.renderElements()
 
 // Создаем объект всплывающего окна с формой добавления карточки
 const popupAddCard = new PopupWithForm(popupAddCardSelector, handleAddCardFormSubmit)
+const popupEditProfile = new PopupWithForm(popupEditProfileSelector, handleProfileFormSubmit)
 
 // Включаем валидацию
-const popupElement = popupAddCard.getElement()
-const formEl = popupElement.querySelector('.form_type_add-card')
-const validatorAddForm = new FormValidator(validateOptions, formEl)
-validatorAddForm.enableValidation()
+const validatorAddForm = enableValidation(popupAddCard, validateOptions)
+const validatorEditProfile = enableValidation(popupEditProfile, validateOptions)
 
 // Инициализация слушателей событий
-formEditProfileElements.form.addEventListener('submit', handleProfileFormSubmit)
-buttonElements.editProfile.addEventListener('click', openEditProfilePopup)
-buttonElements.addCard.addEventListener('click', () => {
+buttonEditProfile.addEventListener('click', () => {
+	validatorEditProfile.toggleButtonState()
+	popupEditProfile.open()
+})
+buttonAddCard.addEventListener('click', () => {
 	validatorAddForm.toggleButtonState()
 	popupAddCard.open()
 })
--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,53 +1,19 @@
-import {Popup} from '../components/Popup.js'
 import {PopupPreviewImage} from '../components/PopupPreviewImage.js'
-// Объекты всплывающих окон редактирования профиля и добавления карточки
-const popupEditProfile = new Popup('.popup_type_edit-profile')
-const popupPreviewPicture = new PopupPreviewImage('.popup_type_image')
-// Элементы всплывающих окон
-const popupEditProfileElement = popupEditProfile.getElement()
-// Элементы блока профиля
+
 const profile = document.querySelector('.profile')
-const name = profile.querySelector('.profile__name')
-const about = profile.querySelector('.profile__about')
-const btnEditProfile = profile.querySelector('.btn_type_edit-profile')
-const btnAddCard = profile.querySelector('.btn_type_add-card')
 
-// Элементы формы редактирования профиля
-const formEditProfile = popupEditProfileElement.querySelector('.form')
-const inputName = formEditProfile.querySelector('.form__input_type_name')
-const inputAbout = formEditProfile.querySelector('.form__input_type_about')
-
-/* ------------ EXPORTS --------------- */
 // Cелектор списка карточек
 export const cardsListSelector = '.cards__list'
-
 // Селектор шаблона карточки
 export const cardTemplateSelector = '#cardTemplate'
-
+// Селекторы всплывающих окон
 export const popupAddCardSelector = '.popup_type_add-card'
-
-// Элементы формы редактирования профиля
-export const formEditProfileElements = {
-  form: formEditProfile,
-  name: inputName,
-  about: inputAbout
-}
-
-// Элементы профиля
-export const profileElements = {name, about}
-
-// Всплывающие окна
-export const popupElements = {
-  editProfile: popupEditProfile,
-  previewPicture: popupPreviewPicture,
-}
-
+export const popupEditProfileSelector = '.popup_type_edit-profile'
+// Всплывающее окно с изображением
+export const popupPreviewPicture = new PopupPreviewImage('.popup_type_image')
 // Кнопки
-export const buttonElements = {
-  editProfile: btnEditProfile,
-  addCard: btnAddCard
-}
-
+export const buttonEditProfile = profile.querySelector('.btn_type_edit-profile')
+export const buttonAddCard = profile.querySelector('.btn_type_add-card')
 // Селекторы для валидации
 export const validateOptions = {
   inputSelector: '.form__input', // Поле ввода
